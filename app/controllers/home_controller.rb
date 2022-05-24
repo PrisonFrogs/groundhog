6c9b00bddbc15b9e64c0a310c74391a04ae1a838
root <root@DESKTOP-2GE0PV8.localdomain>
2022-05-24 17:51:37 +1000

    fix syntax

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,14 +2,11 @@ class HomeController < ApplicationController
   def index
     a = 1
     b = 2
-    c = 0
-
     c = if a > b
           a
         else
           b
         end
-    render json: {xianshi: c}
-    
+    render json: { xianshi: c }
   end
 end
