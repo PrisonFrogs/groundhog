5ed8b098ad0a3a0c5d47bcac8fceea61905d1698
uooobarry <383435763@qq.com>
2022-05-27 17:01:37 +0800

    Add user spec

--- spec/requests/authentication_spec.rb
+++ spec/requests/authentication_spec.rb
@@ -1,7 +1,10 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.describe "Authentications", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
-  end
-end
+# RSpec.describe 'Authentications', type: :request do
+#   describe 'GET /shake' do
+#     it 'returns 200 when not login' do
+#       get '/api/auth/shake'
+#       expect(response).to be_success
+#     end
+#   end
+# end
