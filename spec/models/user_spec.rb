5677b790dce9fd778cfe34665f5ed2eb25788589
UoooBarry <37783883+UoooBarry@users.noreply.github.com>
2022-05-31 14:35:49 +0800

    Feature/jwt authentication (#8)
    * Add JWT authentication
    
    * Add user spec
    
    * Add controller spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'must have an email' do
+    expect(User.new).to be_invalid
+  end
 end
