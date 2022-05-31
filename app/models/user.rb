4b717303720c8de9e271baa68c3dc179c489c3bb
root <root@DESKTOP-2GE0PV8.localdomain>
2022-05-31 17:39:29 +1000

    fix password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   validates :email, :nickname, :password_digest, presence: true
-  validates :password_digest, length: { minimum: 8 }
+  validates :password, length: { minimum: 8 }
   validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   enum gender: {
     secret: 0,
