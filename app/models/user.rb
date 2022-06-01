4ff0de0f68968e4d20dba214d6853ae740342aa6
Tom Cat <43158065+s3713572@users.noreply.github.com>
2022-06-01 17:15:54 +1000

    Feature/user modal validation (#11)
    * add validations in user model and unit test for user model
    
    * fix password validation
    
    * fixed unit testing
    
    Co-authored-by: root <root@DESKTOP-2GE0PV8.localdomain>
    Co-authored-by: uooobarry <383435763@qq.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_secure_password
-  validates :email, :nickname, :password, presence: true
+  validates :email, :nickname, :password_digest, presence: true
+  validates :password, length: { minimum: 8 }
+  validates :email, format: /\w+@\w+\.{1}[a-zA-Z]{2,}/
   enum gender: {
     secret: 0,
     male: 1,
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'must have an email' do
-    expect(User.new).to be_invalid
+  it 'is valid with valid user' do
+    expect(User.new(email: 'test@test.com',
+                    nickname: 'Joey',
+                    password: 'pwd123123',
+                    bio: 'I am the king',
+                    gender: 2,
+                    age: 22,
+                    location: '300 Swanston St, Melbourne VIC, 3000')).to be_valid
+  end
+
+  it 'is not valid without a email' do
+    expect(User.new(email: nil)).to_not be_valid
+  end
+
+  it 'is not valid without a nickname' do
+    expect(User.new(nickname: nil)).to_not be_valid
+  end
+
+  it 'is not valid without a password' do
+    expect(User.new(password_digest: nil)).to_not be_valid
+  end
+
+  it 'is not valid with a incorrect email formate' do
+    expect(User.new(email: '123')).to_not be_valid
   end
 end
