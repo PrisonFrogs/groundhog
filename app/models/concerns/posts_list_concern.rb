dbc527099b2043970064366721703946013f0a18
uooobarry <383435763@qq.com>
2022-06-20 15:37:01 +0800

    Tweak post list

--- app/models/concerns/posts_list_concern.rb
+++ app/models/concerns/posts_list_concern.rb
@@ -2,7 +2,11 @@ module PostsListConcern
   extend ActiveSupport::Concern
 
   included do
-    scope :list_on_homepage, -> { where('created_at > ?', 3.days.ago).where.not(title: nil).order(updated_at: :desc) }
+    scope :list_on_homepage, lambda {
+                               where('created_at > ?', Post.last.created_at - 3.days)
+                                 .where.not(title: nil)
+                                 .order(updated_at: :desc)
+                             }
 
     def as_payload(user = nil)
       {
